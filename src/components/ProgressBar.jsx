b893b303f7197b2534604f0ec6541292ddbadfc2
Keckich <nik.malashko@mail.ru>
2024-01-07 18:57:48 +0300

    added ProgressBar component

---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+export default function ProgressBar({ timer }) {
+  const INTERVAL_TIME = 10;
+  const [remainingTime, setRemainingTime] = useState(timer);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((prev) => prev - INTERVAL_TIME);
+    }, INTERVAL_TIME);
+
+    return () => {
+      clearInterval(interval);
+    }
+  }, []);
+}
